f6f888d551f3ff93e8fe418ae14dd9773ce339c0
SiNiKin <tlsdl99@gmail.com>
2024-06-11 20:08:20 +0900

    ✨ MBTI Front End
    MBTI Front End

--- src/main/java/com/example/modi/MbtiController.java
+++ src/main/java/com/example/modi/MbtiController.java
@@ -6,8 +6,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 @Log4j2
 @RequiredArgsConstructor
 @RequestMapping("/mbti")
@@ -16,7 +20,8 @@ public class MbtiController {
     private final MbtiService mbtiService;
 
     @PostMapping(value = "/result")
-    public String calmbti(@RequestBody MbtiDTO mbtiDto){
+    public Map<String, String> calmbti(@RequestBody MbtiDTO mbtiDto){
+        Map<String, String> resultMap = new HashMap<>();
         // dto로 받아온 데이터로 service한테 mbti를 계산하게 하고, 결과 값을 받아옴.
         String result = mbtiService.calResult(mbtiDto);
 
@@ -26,6 +31,8 @@ public class MbtiController {
         // Service한테 넘겨서 Repository한테 Entity를 DB안에 저장하게 함.
         mbtiService.save(mbtiResult);
 
-        return result;
+        log.info(result + " 안녕 클레오 파트라");
+        resultMap.put("result", result);
+        return resultMap;
     }
 }
